8bfd396879958fe76a958774ae38fb642bbbd96d
Sivasai Pallapu <pallapu.sivasai.33@gmail.com>
2024-11-08 12:33:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed=new typed(".text",{
+    Strings:["Fronted Developer","Backend Developer","Java Developer","Python Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+
+})
